421763489be08a848fad3eacd18c648d33d085f5
philburrrt <philbertburrow@protonmail.com>
2023-09-30 04:35:30 -0500

    fix schema typescript errors

--- apps/client/lib/db/schema.ts
+++ apps/client/lib/db/schema.ts
@@ -26,12 +26,12 @@ import { NANOID_LENGTH } from "./nanoid";
 export const content = mysqlTable(
   "content",
   {
-    createdAt: timestamp("created_at").defaultNow(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
     description: varchar("description", { length: MAX_DESCRIPTION_LENGTH }),
     id: serial("id").primaryKey(),
     owner: varchar("owner", { length: ETH_ADDRESS_LENGTH }).notNull(),
     shareId: bigint("share_id", { mode: "number" }).notNull(),
-    updatedAt: timestamp("updated_at").defaultNow().onUpdateNow(),
+    updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
     url: varchar("url", { length: 255 }).notNull(),
   },
   (table) => ({
@@ -69,7 +69,7 @@ export const repost = mysqlTable(
     quote: varchar("quote", { length: 140 }),
     referenceRepost: bigint("reference_repost", { mode: "number" }), // if it's a repost of a repost
     referenceShareId: bigint("share_id", { mode: "number" }).notNull(), // all should have a shareId
-    createdAt: timestamp("created_at").defaultNow(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
   },
   (table) => ({
     shareIdIndex: uniqueIndex("shareId").on(table.referenceShareId),
