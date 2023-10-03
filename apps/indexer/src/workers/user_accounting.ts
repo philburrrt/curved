0c193cf8e7d453801452fccf752c09c65cd34c2b
philburrrt <philbertburrow@protonmail.com>
2023-10-03 07:27:16 -0500

    findFirst -> select. fix mock-event script

--- apps/indexer/src/workers/user_accounting.ts
+++ apps/indexer/src/workers/user_accounting.ts
@@ -2,7 +2,7 @@
 import { parentPort } from "worker_threads";
 import { db } from "../DB";
 import { userBalances, trade } from "db";
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 
 // receives trade and sharecreaed event
 // if sharecreated, add 1 to creators share balance in userBalances table
@@ -38,45 +38,62 @@ parentPort!.on("message", async (event: any) => {
     console.log("[user_accounting] processing trade", trade);
 
     // ! bug: this returns the owner balance even if querying the trader. seems like the `where` clause is incorrect
-    const existingBalance = await db.query.userBalances.findFirst({
-      columns: {
-        address: true,
-        shareId: true,
-        balance: true,
-      },
-      where: (row, { eq }) =>
-        eq(row.address, trade.from) && eq(row.shareId, shareId),
-    });
+    let existingBalance;
+    try {
+      existingBalance = await db
+        .select()
+        .from(userBalances)
+        .where(
+          and(
+            eq(userBalances.address, trade.from),
+            eq(userBalances.shareId, shareId),
+          ),
+        );
+    } catch (e) {
+      console.error("error querying existing balance", e);
+    }
 
-    if (!existingBalance?.balance) {
-      console.log(
-        "[user_accounting] no existing balance",
-        trade.from.slice(0, 6),
-        shareId,
-      );
-      await db.insert(userBalances).values({
-        address: trade.from,
-        shareId,
-        balance: trade.amount,
-      });
-    } else {
-      console.log(
-        "[user_accounting] existing balance. updating",
-        existingBalance,
-        trade.from.slice(0, 6),
-        shareId,
-      );
-      const newBalance = existingBalance.balance + trade.amount;
-      console.log("[user_accounting] new balance", newBalance);
+    console.log("[user_accounting] existing balance", existingBalance);
+
+    if (!existingBalance)
+      return console.log("[user_accounting] error querying existing balance");
+
+    if (existingBalance.length === 0) {
+      try {
+        await db.insert(userBalances).values({
+          address: trade.from.toLowerCase(),
+          shareId,
+          balance: trade.amount,
+        });
+
+        return;
+      } catch (e) {
+        console.error("error inserting balance", e);
+      }
+    }
+
+    const balance = existingBalance[0]?.balance;
+    if (!balance)
+      return console.log("[user_accounting] error querying existing balance");
+    console.log(
+      "[user_accounting] updating balance",
+      trade.from.slice(0, 6),
+      balance,
+      trade.amount,
+    );
+
+    try {
       await db
         .update(userBalances)
-        .set({
-          balance: newBalance,
-        })
+        .set({ balance: balance + trade.amount })
         .where(
-          eq(userBalances.address, trade.from) &&
+          and(
+            eq(userBalances.address, trade.from),
             eq(userBalances.shareId, shareId),
+          ),
         );
+    } catch (e) {
+      console.error("error updating balance", e);
     }
   }
 });
