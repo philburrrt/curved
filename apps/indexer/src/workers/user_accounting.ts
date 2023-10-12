0fee8493c820ab0994647511e9f25f6e45fae4bc
philburrrt <philbertburrow@protonmail.com>
2023-10-12 13:25:40 -0500

    init

--- apps/indexer/src/workers/user_accounting.ts
+++ apps/indexer/src/workers/user_accounting.ts
@@ -1,8 +1,10 @@
 // worker.js
-import { userBalances } from "db";
+import { nftPost, userBalances } from "db";
 import { and, eq } from "drizzle-orm";
+import { utils } from "ethers";
 import PQueue from "p-queue";
 import { parentPort } from "worker_threads";
+import { sendNotification, Message } from "../sendNotification";
 
 import { db } from "../DB";
 
@@ -20,12 +22,12 @@ const queue = new PQueue({ concurrency: 1 });
 const handleMessage = async (event: any) => {
   const eventType = event.event;
   if (eventType === "ShareCreated") {
-    const owner = event.args[0];
+    const owner = event.args[0].toLowerCase();
     const shareId = event.args[1];
 
     try {
       await db.insert(userBalances).values({
-        address: owner.toLowerCase(),
+        address: owner,
         balance: 1,
         shareId,
       });
@@ -35,12 +37,18 @@ const handleMessage = async (event: any) => {
   } else if (eventType === "Trade") {
     const shareId = event.args[0];
     const side = event.args[1];
-    const trader = event.args[2];
+    const trader = event.args[2].toLowerCase();
     const amount = parseInt(event.args[4]);
+
+    if (side === "0") {
+      const owner = event.args[3].toLowerCase();
+      const price = utils.formatUnits(event.args[5]);
+      handleNotification({ amount, owner, price, shareId, trader });
+    }
 
     const trade = {
       amount: side === "0" ? amount : -amount,
-      from: trader.toLowerCase(),
+      from: trader,
     };
 
     let existingBalance;
@@ -105,6 +113,35 @@ const handleMessage = async (event: any) => {
       console.error("error updating balance", e);
     }
   }
+};
+
+const handleNotification = async (event: any) => {
+  const { shareId, trader, amount, owner, price } = event;
+  const postId = await db
+    .select({
+      postId: nftPost.postId,
+    })
+    .from(nftPost)
+    .where(eq(nftPost.shareId, shareId));
+
+  const msg: Message = {
+    body: `${trader.slice(
+      0,
+      6,
+    )} purchased ${amount} shares of your post for ${price} ETH`,
+    title: `Someone purchased your share!`,
+    url: `https://yuyu.social/post/${postId}`,
+  };
+
+  sendNotification(owner, msg);
+
+  console.log("postId", postId);
+};
+
+export const formatUnits = (value: bigint, decimals = 2) => {
+  const ethValue = utils.formatEther(value);
+  const formatted = Number(ethValue).toFixed(decimals);
+  return formatted;
 };
 
 parentPort.on("message", async (event: any) => {
